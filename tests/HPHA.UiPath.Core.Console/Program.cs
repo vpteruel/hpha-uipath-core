0cb0408bf4f2b6f4a0faf6e7109b75589fbb4a7e
Vinicius Teruel <vpteruel@gmail.com>
2025-03-03 14:26:24 -0500

    Remove Console project and related files; update solution structure and CI workflow to run tests

--- tests/HPHA.UiPath.Core.Console/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using HPHA.UiPath.Core.Converters;
-
-FileInfo fileInfo = new("Json/test1_c.json");
-
-var purchaseOrder = JsonToEntityConverter.ConvertJsonToEntity(fileInfo);
-
-Console.WriteLine($"Invoice ID: {purchaseOrder?.InvoiceId}");
-Console.WriteLine($"PO#.......: {purchaseOrder?.PurchaseOrder}");
